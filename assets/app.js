71f2c629bd5d91b5636c2df86ce40f3979f7e633
Justin Cokeroft <jcokeroft@outlook.com>
2026-06-23 20:16:16 -0400

    feat: unlock OP09, correct name from The Four Emperors to Emperors in the New World

--- assets/app.js
+++ assets/app.js
@@ -11,7 +11,7 @@ const ALL_SETS = [
   { code: 'OP06',  name: 'Wings of the Captain',        group: 'booster', available: false },
   { code: 'OP07',  name: '500 Years in the Future',     group: 'booster', available: false },
   { code: 'OP08',  name: 'Two Legends',                 group: 'booster', available: false },
-  { code: 'OP09',  name: 'The Four Emperors',           group: 'booster', available: false },
+  { code: 'OP09',  name: 'Emperors in the New World',   group: 'booster', available: true  },
   { code: 'OP10',  name: 'Royal Blood',                 group: 'booster', available: true  },
   { code: 'OP11',  name: 'A Fist of Divine Speed',      group: 'booster', available: true  },
   { code: 'OP12',  name: 'Legacy of the Master',        group: 'booster', available: true  },
